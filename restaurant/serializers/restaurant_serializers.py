43ead52d80422ed9d58ef52a84b1da44f83aee55
ShinSub98 <wjdtjq26@naver.com>
2024-02-26 12:20:50 +0900

    feat: 메뉴 시리얼라이저 추가

--- restaurant/serializers/restaurant_serializers.py
+++ restaurant/serializers/restaurant_serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from ..models import *
 
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = '__all__'
+
+
 class RestaurantRequestSerailizer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
@@ -8,6 +14,7 @@ class RestaurantRequestSerailizer(serializers.ModelSerializer):
 
     
 class RestaurantResponseSerializer(serializers.ModelSerializer):
+    menu = MenuSerializer(many = True)
     class Meta:
         model = Restaurant
         exclude = ['score_accum']
